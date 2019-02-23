eeb46bfa7cf59552d6d028d724ec3018e1bd3280
ghostrib <sh4rkbot@gmail.com>
2019-02-23 02:58:04 -0600

    completes evenOccurence

--- Arrays/evenOccurence.js
+++ Arrays/evenOccurence.js
@@ -8,3 +8,21 @@ evenOccurence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]) // 2
 evenOccurence([ "cat", "dog", "dig", "cat" ]) // "cat"
 */
 
+const evenOccurrence = arr => {
+    let obj = {};
+    let result;
+    for (let item of arr) {
+        obj['item-' + item] = obj['item-' + item] + 1 || 1;
+    }
+
+    for (let key in obj) {
+        if (obj[key] % 2 === 0) {
+            result = key.substr(5);
+            if (result % 1 === 0) {
+                result = Number(result)
+            }
+            return result
+        }
+    }
+    return null
+}
