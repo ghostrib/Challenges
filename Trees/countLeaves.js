bc0d4b4ed107c0f50ad9d9cef328534c102042e2
ghostrib <sh4rkbot@gmail.com>
2019-02-23 03:27:05 -0600

    adds countLeaves

---
+++ Trees/countLeaves.js
@@ -0,0 +1,29 @@
+/*
+Implement the countLeaves function in this Tree class.
+
+A leaf node is any node in the tree that has no children. countLeaves should
+traverse the tree, and return the number of leaf nodes the tree contains.
+
+Illustration of a tree with three leaves:
+
+      * <- root
+     / \
+    *    * <- leaf
+   / \
+  *   * <- leaf
+ /
+* <- leaf
+Example usage:
+
+var root = new Tree();
+root.countLeaves(); // 1
+root.addChild(new Tree());
+root.countLeaves(); // still 1
+root.addChild(new Tree());
+root.children[0].addChild(new Tree());
+root.children[0].addChild(new Tree());
+root.children[0].children[0].addChild(new Tree());
+root.countLeaves(); // 3
+
+*/
+
