ca886decf064be1dff675ec34fc68222602f9152
ghostrib <sh4rkbot@gmail.com>
2019-02-23 02:56:37 -0600

    adds evenOccurence

---
+++ Arrays/evenOccurence.js
@@ -0,0 +1,10 @@
+/*
+Find the first item that occurs an even number of times in an array.
+Remember to handle multiple even-occurrence items and return the first one.
+Return null if there are no even-occurrence items.
+
+Examples
+evenOccurence([ 1, 3, 3, 3, 2, 4, 4, 2, 5 ]) // 2
+evenOccurence([ "cat", "dog", "dig", "cat" ]) // "cat"
+*/
+
