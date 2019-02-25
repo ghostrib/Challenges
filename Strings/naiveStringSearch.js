f3e1fdaf687675b397e19002a36ecfaed4ab8059
ghostrib <sh4rkbot@gmail.com>
2019-02-25 02:16:12 -0600

    completes naiveStringSearch

--- Strings/naiveStringSearch.js
+++ Strings/naiveStringSearch.js
@@ -3,3 +3,18 @@ Write a function which takes two strings, a long string and a short string
 and returns the number of times the short string appears in the longer string
 */
 
+const stringSearch = (long, short) => {
+    let count = 0;
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (short[j] !== long[i + j]) {
+                break;
+            }
+            if (j === short.length - 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
