e94157057143e0c0ce8ab0e7d5146e905caad145
ghostrib <sh4rkbot@gmail.com>
2019-02-25 03:15:52 -0600

    completes firstNonRepeatedChar

--- Strings/firstNonRepeatedChar.js
+++ Strings/firstNonRepeatedChar.js
@@ -3,3 +3,18 @@ Given an arbitrary input string, return the first non-repeating character.
 For strings with all repeats, return 'sorry'.
 */
 
+const mapChars = string => {
+    const map = {};
+    for (let char of string) {
+        map[char] = map[char] + 1 || 1;
+    }
+    return map;
+}
+
+const firstNonRepeatedCharacter = string => {
+    const map = mapChars(string);
+    for (let char of string) {
+        if (map[char] === 1) return char;
+    }
+    return 'sorry'
+}
