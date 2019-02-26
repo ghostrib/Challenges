f73a78649f667d45197f6561b0597cbfd5810bb2
ghostrib <sh4rkbot@gmail.com>
2019-02-26 00:43:31 -0600

    completes es6 class implementation

--- Trees/treeMap.js
+++ Trees/treeMap.js
@@ -10,62 +10,62 @@ but it should NOT modify the tree that was passed in.
 */
 
 // Basic tree that stores a value
-var Tree = function (value) {
-    this.value = value;
-    this.children = [];
-};
-
-Tree.prototype.map = function (callback) {
-    const tree = new Tree(callback(this.value));
-    for (let i = 0; i < this.children.length; i++) {
-        tree.children.push(this.children[i].map(callback));
-    }
-    return tree;
-};
-
-// add an immediate child (wrap values in Tree nodes if they're not already)
-Tree.prototype.addChild = function (child) {
-    if (!child || !(child instanceof Tree)) {
-        child = new Tree(child);
+class Tree {
+    constructor(value) {
+        this.value = value;
+        this.children = [];
     }
 
-    if (!this.isDescendant(child)) {
-        this.children.push(child);
-    } else {
-        throw new Error("That child is already a child of this tree");
-    }
-    // return the new child node for convenience
-    return child;
-};
-
-// check to see if the provided tree is already a child of this tree or any of its sub trees
-Tree.prototype.isDescendant = function (child) {
-    if (this.children.indexOf(child) !== -1) {
-        // `child` is an immediate child of this tree
-        return true;
-    } else {
-        for (var i = 0; i < this.children.length; i++) {
-            if (this.children[i].isDescendant(child)) {
-                // `child` is descendant of this tree
-                return true;
-            }
+    map(callback) {
+        const tree = new Tree(callback(this.value));
+        for (let i = 0; i < this.children.length; i++) {
+            tree.children.push(this.children[i].map(callback));
         }
-        return false;
+        return tree;
     }
-};
 
-// remove an immediate child
-Tree.prototype.removeChild = function (child) {
-    var index = this.children.indexOf(child);
-    if (index !== -1) {
-        // remove the child
-        this.children.splice(index, 1);
-    } else {
-        throw new Error("That node is not an immediate child of this tree");
+    // add an immediate child (wrap values in Tree nodes if they're not already)
+    addChild(child) {
+        if (!child || !(child instanceof Tree)) {
+            child = new Tree(child)
+        }
+        if (!this.isDescendant(child)) {
+            this.children.push(child);
+        } else {
+            throw new Error("That child is already a child of this tree");
+        }
+        // return the new child node for convenience
+        return child;
     }
-};
 
-/*
+    // check to see if the provided tree is already a child of this tree or any of its sub trees
+    isDescendant(child) {
+        if (this.children.indexOf(child) !== -1) {
+            // child is an immediate child of this tree
+            return true;
+        } else {
+            for (let i = 0; i < this.children.length; i++) {
+                if (this.children[i].isDescendant(child)) {
+                    // child is descendant of this tree
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    // remove an immediate child
+    removeChild(child) {
+        const index = this.children.indexOf(child);
+        if (index !== -1) {
+            this.children.splice(index, 1);
+        } else {
+            throw new Error("That node is not an immediate child of this tree");
+        }
+    }
+
+}
+
 var input = new Tree(1);
 // depth: 1
 input.addChild(2);
@@ -83,4 +83,3 @@ var double = function (value) { return value * 2; };
 var result = input.map(double);
 
 console.log(result)
-*/
