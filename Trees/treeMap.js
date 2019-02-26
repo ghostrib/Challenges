0f44f3f0cd6e35e111039b5c8393dd9f0fe0cba6
ghostrib <sh4rkbot@gmail.com>
2019-02-26 00:07:33 -0600

    adds treeMap

---
+++ Trees/treeMap.js
@@ -0,0 +1,10 @@
+/*
+Implement a map method on this Tree class.
+
+Map accepts a mapping function as its only argument.
+It traverses the tree, passing each node’s value into the mapping function,
+and generates a new tree containing the results.
+
+So map should return a tree with the same structure, and different values,
+but it should NOT modify the tree that was passed in.
+*/
