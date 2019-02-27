7f8f09c5e81b10e17a8a0931a998186e94f9275a
ghostrib <sh4rkbot@gmail.com>
2019-02-27 00:08:56 -0600

    completes arrayception

--- Arrays/arrayception.js
+++ Arrays/arrayception.js
@@ -3,7 +3,26 @@ Given an array of arbitrarily nested arrays, return n,
 where n is the deepest level that contains a non-array value.
 */
 
-
+const arrayception = array => {
+    let count;
+    let depth = 0;
+    for (let item of array) {
+        if (item.length === 0) {
+            count = 0;
+        } else if (Array.isArray(item)) {
+            count = arrayception(item);
+            if (count > 0) {
+                count += 1;
+            }
+        } else {
+            count = 1;
+        }
+        if (count > depth) {
+            depth = count;
+        }
+    }
+    return depth
+}
 
 /*
 arrayception([ [ 5 ], [ [ ] ] ]) // 2
