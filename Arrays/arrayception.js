3defdb6f7dd74dfb0c800b07e693ce29f47d9f85
ghostrib <sh4rkbot@gmail.com>
2019-02-27 00:07:49 -0600

    adds examples

--- Arrays/arrayception.js
+++ Arrays/arrayception.js
@@ -3,3 +3,12 @@ Given an array of arbitrarily nested arrays, return n,
 where n is the deepest level that contains a non-array value.
 */
 
+
+
+/*
+arrayception([ [ 5 ], [ [ ] ] ]) // 2
+arrayception([ 10, 20, 30, 40 ]) // 1
+arrayception([ [ 10, 20 ], [ [ 30, [ 40 ] ] ] ]) // 4
+arrayception([ ]) // 0
+arrayception([ [ [ ] ] ]) // 0
+*/
