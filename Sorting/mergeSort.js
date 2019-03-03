55f3e8adbdd3607d129d9dd8eb3c25176ee0b503
ghostrib <sh4rkbot@gmail.com>
2019-03-03 00:38:39 -0600

    adds mergeSort

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,15 @@
+/*
+Implement a function that sorts an array of numbers using the “mergesort” algorithm.
+
+Mergesort uses a divide-and-conquer strategy.
+It begins by treating the input list of length N as a set of N “sublists” of length 1,
+which are considered to be sorted.
+
+Adjacent sublists are then “merged” into sorted sublists of length 2,
+which are merged into sorted sublists of length 4, and so on,
+until only a single sorted list remains.
+(Note, if N is odd, an extra sublist of length 1 will be left after the first merge, and so on.)
+
+This can be implemented using either a recursive (“top-down”) or an iterative (“bottom-up”) approach.
+*/
+
