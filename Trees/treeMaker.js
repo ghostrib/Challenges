c04bfbde54610d96f5c0d3ac7561bbf25c515d0f
ghostrib <sh4rkbot@gmail.com>
2019-03-10 04:22:02 -0500

    adds treeMaker.js

---
+++ Trees/treeMaker.js
@@ -0,0 +1,7 @@
+/*
+
+Someone wrote a tree implementation, b
+ut they forgot to finish writing the definitions for addChild and contains.
+
+Help this person finish their tree by adding in the missing code for these methods.
+*/
