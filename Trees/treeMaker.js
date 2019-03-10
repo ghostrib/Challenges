6c22b438abfee448543928215bf8537e5cd935e7
ghostrib <sh4rkbot@gmail.com>
2019-03-10 04:23:01 -0500

    adds initial treeMaker function

--- Trees/treeMaker.js
+++ Trees/treeMaker.js
@@ -5,3 +5,11 @@ ut they forgot to finish writing the definitions for addChild and contains.
 
 Help this person finish their tree by adding in the missing code for these methods.
 */
+
+var treeMaker = function (value) {
+    var newTree = Object.create(treeMaker.methods);
+    newTree.value = value;
+    newTree.children = [];
+    return newTree;
+};
+treeMaker.methods = {};
