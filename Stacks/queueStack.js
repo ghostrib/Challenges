d1bffb0386d2c4736214a85a3c141c8b6ce31dc5
ghostrib <sh4rkbot@gmail.com>
2019-03-11 04:58:28 -0500

    adds stack functionality

--- Stacks/queueStack.js
+++ Stacks/queueStack.js
@@ -3,3 +3,20 @@ Write a stack.
 Once you’re done, implement a queue using two stacks to create FIFO behavior.
 The queue should not have any storage separate from its stacks.
 */
+
+var Stack = function () {
+    var storage = [];
+
+    this.push = function (value) {
+        storage.push(value)
+    };
+
+    this.pop = function () {
+        return storage.pop()
+    };
+
+    this.size = function () {
+        return storage.length
+    };
+};
+
