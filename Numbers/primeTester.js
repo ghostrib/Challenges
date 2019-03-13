ac93973a55e4cf0fb3484e651670912c49316462
ghostrib <sh4rkbot@gmail.com>
2019-03-13 00:06:30 -0500

    adds primeTester

---
+++ Numbers/primeTester.js
@@ -0,0 +1,6 @@
+/*
+Prime Tester
+A prime number is an integer greater than 1 that has no divisors other than itself and 1.
+Write a function that accepts a number and returns true if it’s a prime number, false if it’s not.
+*/
+
