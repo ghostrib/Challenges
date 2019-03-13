21f2597e80bc3a37038e7069cbe14ad650edaeea
ghostrib <sh4rkbot@gmail.com>
2019-03-13 00:07:18 -0500

    completes primeTester

--- Numbers/primeTester.js
+++ Numbers/primeTester.js
@@ -4,3 +4,11 @@ A prime number is an integer greater than 1 that has no divisors other than itse
 Write a function that accepts a number and returns true if it’s a prime number, false if it’s not.
 */
 
+function primeTester(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return n !== 1 && n !== 0;
+}
