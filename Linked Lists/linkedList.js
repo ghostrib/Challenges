4092f28d2eab81d516b56c5eb0be17873670d91a
ghostrib <sh4rkbot@gmail.com>
2019-03-15 12:44:22 -0500

    completes linkedList

--- Linked Lists/linkedList.js
+++ Linked Lists/linkedList.js
@@ -8,4 +8,22 @@ var LinkedList = function (initialValue) {
     this.tail = this.head;
 };
 
+LinkedList.prototype.addToTail = function (value) {
+    this.tail.next = new Node(value);
+    this.tail = this.tail.next;
+    if (!this.head.value) this.head = this.tail;
+};
 
+LinkedList.prototype.removeHead = function () {
+    this.head = this.head.next;
+    if (!this.head) this.tail = null;
+};
+
+LinkedList.prototype.contains = function (value) {
+    var node = this.head;
+    while (node) {
+        if (node.value === value) return true;
+        node = node.next
+    }
+    return false;
+};
