53a080a9e0c67c2ca99b1bd9bcfb865fb658542c
ghostrib <sh4rkbot@gmail.com>
2019-03-18 22:50:23 -0500

    completes nthFibonacci

--- Numbers/nthFibonacci.js
+++ Numbers/nthFibonacci.js
@@ -2,3 +2,12 @@
 Write a function that accepts a number n and returns the nth fibonacci number.
 */
 
+const nthFibonacci = n => {
+    let num0 = 0, num1 = 1, temp;
+    while (n--) {
+        temp = num1;
+        num1 += num0;
+        num0 = temp;
+    }
+    return num0
+};
