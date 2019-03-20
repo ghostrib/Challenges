4f74b22eb6765497788d6e7ed32c25d9c2b7ed7a
ghostrib <sh4rkbot@gmail.com>
2019-03-20 01:03:57 -0500

    completes longestRun

--- Strings/longestRun.js
+++ Strings/longestRun.js
@@ -6,3 +6,21 @@ If there are two runs of equal length, return the first one.
 Return [0,0] for no runs.
 */
 
+function longestRun(string) {
+    let current = [0, 0];
+    let longest = [0, 0];
+    for (let i = 1; i < string.length; i++) {
+        // if there is a run
+        if (string[i] === string[i - 1]) {
+            current[1] = i;
+            let diffCurrent = current[1] - current[0];
+            let diffLongest = longest[1] - longest[0];
+            if (diffCurrent > diffLongest) {
+                longest = current;
+            }
+        } else {
+            current = [i, i]
+        }
+    }
+    return longest;
+}
