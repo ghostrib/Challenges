599d63d5a5b836291ddf81400a0550f03244c8ee
ghostrib <sh4rkbot@gmail.com>
2019-03-26 21:43:16 -0500

    completes longestPalindrome

--- Strings/longestPalindrome.js
+++ Strings/longestPalindrome.js
@@ -7,3 +7,22 @@ For example, in the string “My dad is a racecar athlete”, the longest palind
 Count whitespaces as valid characters.
 Other palindromes in the above string include “dad”, “ete”, “ dad “ (including whitespace on each side of dad).
 */
+
+function longestPalindrome(string) {
+    let longest = '';
+    for (let i = 0; i < string.length; i++) {
+        for (let j = i; j < string.length; j++) {
+            let substring = string.slice(i, j + 1);
+            if (isPalindrome(substring)) {
+                if (substring.length > longest.length) {
+                    longest = substring;
+                }
+            }
+        }
+    }
+    return longest;
+}
+
+function isPalindrome(string) {
+    return string.split('').reverse().join('') === string;
+}
