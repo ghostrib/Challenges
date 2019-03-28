bc68e874efdf6ccda7ed4c85142d4c1ed31e9fc0
ghostrib <sh4rkbot@gmail.com>
2019-03-27 23:29:50 -0500

    completes largestProductOfThree

--- Numbers/largestProductOfThree.js
+++ Numbers/largestProductOfThree.js
@@ -5,3 +5,9 @@ Write a function that accepts an array of integers
 and returns the largest product possible from three of those numbers.
 */
 
+function largestProductOfThree(array) {
+    array.sort((a, b) => b - a);
+    const product1 = array[0] * array[1] * array[2];
+    const product2 = array[0] * array[array.length - 1] * array[array.length - 2];
+    return Math.max(product1, product2);
+}
