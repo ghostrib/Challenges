742875436aa7ab05bc52290d6974e42263526e8f
ghostrib <sh4rkbot@gmail.com>
2019-04-01 23:31:42 -0500

    completes militaryTime

--- Strings/militaryTime.js
+++ Strings/militaryTime.js
@@ -4,3 +4,17 @@ Given a string that represents time in hours and minutes,
 convert the string to military time (if necessary).
 */
 
+function toMilitary(time) {
+    const digits = time.match(/\d+:\d+/g)[0].split(':')
+    if (time.substr(-2) === 'pm' && digits[0] < 12) {
+        digits[0] = parseInt(digits[0]) + 12;
+    }
+    if (time.substr(-2) === 'am') {
+        if (digits[0] === '12') {
+            digits[0] = '00'
+        } else {
+            digits[0] = '0' + digits[0];
+        }
+    }
+    return digits.join(':')
+}
