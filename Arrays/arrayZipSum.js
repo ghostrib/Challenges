69c91ab788a32845e28c45237566146427df39ff
ghostrib <sh4rkbot@gmail.com>
2019-04-03 22:19:40 -0500

    adds arrayZipSum

---
+++ Arrays/arrayZipSum.js
@@ -0,0 +1,8 @@
+/*
+Array Zip Sum
+
+Given two arrays of negative/positive integers,
+return a new array such that each element at index n
+is the sum of the two elements from the other arrays at their index n’s.
+*/
+
