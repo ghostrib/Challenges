8d4240bb3862b7774100f15488551f5acc3e295a
ghostrib <sh4rkbot@gmail.com>
2019-04-03 22:20:30 -0500

    completes arrayZipSum

--- Arrays/arrayZipSum.js
+++ Arrays/arrayZipSum.js
@@ -6,3 +6,11 @@ return a new array such that each element at index n
 is the sum of the two elements from the other arrays at their index n’s.
 */
 
+function zipSum(arr1, arr2) {
+    const result = [];
+    const shortest = Math.min(arr1.length, arr2.length);
+    for (let i = 0; i < shortest; i++) {
+        result[i] = arr1[i] + arr2[i];
+    }
+    return result;
+}
