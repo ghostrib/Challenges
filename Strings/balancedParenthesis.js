242cfe32f1ae5500dded2f82ab8cf402b4a18524
ghostrib <sh4rkbot@gmail.com>
2019-04-04 23:48:42 -0500

    completes balancedParenthesis

--- Strings/balancedParenthesis.js
+++ Strings/balancedParenthesis.js
@@ -3,3 +3,13 @@ Balanced Parenthesis
 Given a string, return true if it contains balanced parenthesis ().
 */
 
+const isBalanced = str => {
+    let count = 0;
+    for (let char of str) {
+        if (char === '(') count += 1;
+        if (char === ')') count -= 1;
+        if (count < 0) return false;
+    }
+    return count === 0;
+};
+
