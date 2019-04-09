c0c4f6f7da4b810c7089d455f6afb1da8405e163
ghostrib <sh4rkbot@gmail.com>
2019-04-09 04:24:13 -0500

    completes filterPalindromes

--- Arrays/filterPalindromes.js
+++ Arrays/filterPalindromes.js
@@ -3,3 +3,25 @@ Filter Palindromes
 Given an array of words (array of strings), return all words that are palindromes.
 */
 
+const filterPalindromes = words => {
+    const result = [];
+    for (let word of words) {
+        if (isPalindrome(word)) {
+            result.push(word)
+        }
+    }
+    return result;
+}
+
+const getAlphaChars = str => {
+    return str.toLowerCase().match(/\w+/g).join('');
+}
+
+const reverseString = str => {
+    return str.split('').reverse().join('');
+}
+
+const isPalindrome = str => {
+    const copy = getAlphaChars(str);
+    return copy === reverseString(copy);
+}
