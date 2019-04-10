26c0c558ca8f20f1ee1eb6c7a61e0bc5fa1842b6
ghostrib <sh4rkbot@gmail.com>
2019-04-10 13:46:36 -0500

    adds ponyFarm

---
+++ Arrays/ponyFarm.js
@@ -0,0 +1,8 @@
+/*
+Pony Farm
+A certain pony farm (cage-free) raises and keeps track of cowboys’ and cowgirls’ ponies.
+Each pony has a name, id, dietary allergies, and email address (the owner’s email address).
+
+Given an array of pony objects and an owner’s email address,
+return an alphabetically ordered array of all foods (without duplicates) that the owner should avoid feeding his/her ponies.
+*/
