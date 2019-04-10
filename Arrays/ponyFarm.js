cd316c04cc29acc3afa7d2ef758033a183364cc1
ghostrib <sh4rkbot@gmail.com>
2019-04-10 13:47:27 -0500

    completes ponyFarm

--- Arrays/ponyFarm.js
+++ Arrays/ponyFarm.js
@@ -6,3 +6,15 @@ Each pony has a name, id, dietary allergies, and email address (the owner’s em
 Given an array of pony objects and an owner’s email address,
 return an alphabetically ordered array of all foods (without duplicates) that the owner should avoid feeding his/her ponies.
 */
+
+const getPonyAllergies = (ponies, ownerEmail) => {
+    const allergies = [];
+    for (let i = 0; i < ponies.length; i++) {
+        if (ponies[i].email === ownerEmail) {
+            ponies[i].allergies.forEach(item => {
+                if (!allergies.includes(item)) allergies.push(item)
+            })
+        }
+    }
+    return allergies.sort();
+}
