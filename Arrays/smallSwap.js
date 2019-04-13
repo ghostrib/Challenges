73fdd12b01f382c195e3bfd0b49a546d90f853db
ghostrib <sh4rkbot@gmail.com>
2019-04-12 22:28:32 -0500

    completes smallSwap

--- Arrays/smallSwap.js
+++ Arrays/smallSwap.js
@@ -5,4 +5,15 @@ Given an array of negative/positive integers,
 swap the smallest element of the array with the first element of the array.
 */
 
-
+function smallSwap(numbers) {
+    if (numbers.length === 0) return numbers;
+    let smallest = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] < smallest) smallest = numbers[i];
+    }
+    const toSwap = numbers.indexOf(smallest);
+    const first = numbers[0];
+    numbers[0] = smallest;
+    numbers[toSwap] = first;
+    return numbers;
+}
