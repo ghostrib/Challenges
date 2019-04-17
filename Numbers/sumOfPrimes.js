287bf326424bb3eff71cf5ef7f4eafbf15907eb3
ghostrib <sh4rkbot@gmail.com>
2019-04-16 23:08:46 -0500

    completes sumOfPrimes

--- Numbers/sumOfPrimes.js
+++ Numbers/sumOfPrimes.js
@@ -3,3 +3,18 @@ Sum of Primes
 Given a positive number, return the sum of all positive primes that are less than or equal to that number.
 */
 
+function sumOfPrimes(n) {
+    const primes = [];
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) primes.push(i)
+    }
+    return primes.reduce((a, b) => a + b, 0)
+}
+
+function isPrime(n) {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+    return n !== 1 && n !== 0;
+}
+
