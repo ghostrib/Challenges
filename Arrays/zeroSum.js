1c5b2ea70093c6e5baa72fd2243875b733292d51
ghostrib <sh4rkbot@gmail.com>
2019-04-18 15:50:47 -0500

    completes zeroSum

--- Arrays/zeroSum.js
+++ Arrays/zeroSum.js
@@ -3,3 +3,13 @@ Zero Sum
 Given an array of negative/positive integers, return true if there exists two numbers whose sum is zero.
 */
 
+function zeroSum(numbers) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === 0) return true;
+        sum < 0 ? left++ : right--
+    }
+    return false
+}
