2fb42803f213e219f87f0ce3f8736f9d935968d0
ghostrib <sh4rkbot@gmail.com>
2019-04-22 22:50:54 -0500

    refactors bubbleSort using array destructuring

--- Sorting/bubbleSort.js
+++ Sorting/bubbleSort.js
@@ -10,26 +10,19 @@ Once it gets to the end of the array, it starts over and repeats the process unt
 Implement a function that takes an array and sorts it using this technique.
 */
 
-const swap = (array, index1, index2) => {
-    const temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
-}
-
 const bubbleSort = array => {
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < array.length; i++) {
-            let j = i + 1;
-            if (array[i] > array[j]) {
-                swap(array, i, j);
-                swapped = true;
-            }
-        }
-    } while (swapped);
-    return array;
-}
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return array;
+};
 
-//const arr = [7, 38, 48, 3, 42, 14, 84, 2, 15, 68, 83, 22, 9, 18, 33, 92, 1, 39];
-//console.log(bubbleSort(arr)); // [ 1, 2, 3, 7, 9, 14, 15, 18, 22, 33, 38, 39, 42, 48, 68, 83, 84, 92 ];
+// const arr = [7, 38, 48, 3, 42, 14, 84, 2, 15, 68, 83, 22, 9, 18, 33, 92, 1, 39];
+// console.log(bubbleSort(arr)); // [ 1, 2, 3, 7, 9, 14, 15, 18, 22, 33, 38, 39, 42, 48, 68, 83, 84, 92 ];
