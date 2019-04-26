5f8185cbfc1d975c92b2bcd03bb0ec99d707b62e
ghostrib <sh4rkbot@gmail.com>
2019-04-25 23:14:16 -0500

    completes balancedBrackets

--- Strings/balancedBrackets.js
+++ Strings/balancedBrackets.js
@@ -4,3 +4,17 @@ Given a string, return true if its arrangement of
 parenthesis (), curly-brackets {}, and square-brackets [] 
 would be considered valid in code and mathematics (is balanced).
 */
+
+function isBalanced(str) {
+  const open = { "(": ")", "[": "]", "{": "}" };
+  const close = { ")": "(", "}": "{", "]": "[" };
+  const stack = [];
+  for (let i of str) {
+    if (open[i]) stack.push(i);
+    if (close[i]) {
+      if (stack.pop() !== close[i]) return false;
+    }
+  }
+  if (stack.length === 0) return true;
+  return false;
+}
